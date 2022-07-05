39877476a2682e9857c407998f81bf5411ec7e17
AK3377 <108611700+AK3377@users.noreply.github.com>
2022-07-05 22:21:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,57 +13,37 @@ function bmicard(){
      h=h/100 
      
      var result=w/(h*h)
-     console.log(+result)
-     result=result.toFixed(2)
+      result=result.toFixed(2)
+       document.getElementById("screen1").value=result;
     
-     document.getElementById("screen1").value=result;
-    
-    
-    
-     if(result>=40){
-        
-        console.log("Extreme Obesity ")
-        document.getElementById("screen2").value="Extreme Obesity"
+         if(result>=40){
+            document.getElementById("screen2").value="Extreme Obesity"
     
       } else if(result>=30){
-            console.log("Obese")
-           
             document.getElementById("screen2").value="Obese"
       } 
     else if(result>=25){
-    console.log( "Overweight ");
-    document.getElementById("screen2").value="Overweight "
+        document.getElementById("screen2").value="Overweight "
     }
     else if(result<=16){
-        console.log( "Severely Underweight");
-        document.getElementById("screen2").value="Severely Underweight"
+         document.getElementById("screen2").value="Severely Underweight"
     }
       else if(result<=18.5){
-        console.log(" Underweight");
-        document.getElementById("screen2").value="Underweight"
+          document.getElementById("screen2").value="Underweight"
       }else{
-        console.log("Normal");
-        document.getElementById("screen2").value="Normal"
+          document.getElementById("screen2").value="Normal"
+    }
+        
     }
     
-     
-    }
-    
-    
-    
-    function clearbuton(){
+        function clearbuton(){
         document.getElementById("screen1").value=""
         document.getElementById("screen2").value=""
         document.getElementById("cm").value=""
         document.getElementById("kg").value=""
-      
-        
+             
     }
-     
-    
-    
-    
-    
+       
     
     function bmiclose(){
         var c=document.getElementById("bmi");
